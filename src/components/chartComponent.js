42a75cdf6fad8314b001acbbe517381160969425
DilipCoder <krdilip276@gmail.com>
2019-07-20 15:14:37 +0530

    customise react pie chart

--- src/components/chartComponent.js
+++ src/components/chartComponent.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import {Pie} from 'react-chartjs-2';
+import Modal from './modal';
 
 class ChartDemo extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
+      show: false,
+      labelName:'',
+      labelValue:'',
       labels: ['Bike', 'Car', 'Scooter'],
       datasets: [{
         data: [2000, 4000, 2637],
@@ -15,14 +19,28 @@ class ChartDemo extends React.Component {
     
   }
 
+  showModal = (item) => {
+    let labelName = item[0]._model.label;
+    let labelValue = this.state.datasets[0].data[item[0]._index]
+    this.setState({ show: true, labelName, labelValue });
+  };
+
+  hideModal = () => {
+    
+    this.setState({ show: false,});
+  };
+
  
 
   render() {
-
- function handleClick(e) {
-   e.preventDefault();
-   console.log("click");
- }
+    let showModal = this.showModal
+    let {labelName, labelValue} = this.state
+    let options = {
+      'onClick': function (evt, item) {
+        showModal(item)
+        console.log('legend onClick', evt);
+        console.log('legd item', item);
+      }}
 
     return (
       <div>
@@ -31,9 +49,14 @@ class ChartDemo extends React.Component {
            labels: this.state.labels,
            datasets: this.state.datasets
          }}
-         height = '90%'
-         />
+         options={options}
+         
+/>
          <br />
+        <Modal show={this.state.show} handleClose={this.hideModal}>
+          <p>Label Name: {labelName}</p>
+          <p>Value: {labelValue}</p>
+        </Modal>
       </div>
     )
   }
